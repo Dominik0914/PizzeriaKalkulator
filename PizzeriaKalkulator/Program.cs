d794cd84f2cb2625fe069be52426b007d46e30fc
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-23 12:26:16 +0200

    2.feladat

--- PizzeriaKalkulator/Program.cs
+++ PizzeriaKalkulator/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Kérem a nevedet: ");
+string nev = Console.ReadLine();
+Console.WriteLine("Kérem a rendelés összegét: ");
+int osszeg = int.Parse(Console.ReadLine());
+bool Tvasarlo = true;
+Console.WriteLine("Törzsvásárló vagy?(igen/nem)");
+string valasz = Console.ReadLine();
+
